69061b0304bba0a0417fb986a59ed61abb9f88aa
ClBachman <ClBachman@davidson.edu>
2026-05-28 10:00:27 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,18 +5,19 @@ hello.py
 A tiny collaborative workflow exercise for practicing
 Git + GitHub research development workflows.
 
-author: Your Name
+author: Claire
 date created: 05/28/2026
 """
 
 def main():
     """
-    Print a welcome mesage for the Git workflow activity.
+    Print a welcome message for the Git workflow activity.
     """
 
-    print("Welcom to the collaborative Git workflow activity!")
+    print("Welcome to the collaborative Git workflow activity!")
 
 
 if __name__ == "__main__":
     main()
 ```
+
